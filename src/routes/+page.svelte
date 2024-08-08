d08040a27e7710e77967f79cb6e7a31011250f0a
jorj-kare <jorj.kare@gmail.com>
2024-08-08 21:37:10 +0200

    feat: add admin & furnitures routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header>
+	<div class="title-box">
+		<h1>Αποστόλης Περδίκης</h1>
+		<h2>50 χρόνια πρωτοπόρος στην τέχνη της ξυλουργικής</h2>
+	</div>
+</header>
+
+<style>
+	header {
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background:
+			linear-gradient(to top right, #00000049, #0f030389),
+			url(hero_img.jpg) center/cover no-repeat;
+	}
+	.title-box {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: start;
+		height: max-content;
+		color: #fafafa;
+	}
+	h1 {
+		font-size: clamp(52px, 6vw, 90px);
+		font-weight: 700;
+		font-style: normal;
+	}
+	h2 {
+		font-weight: 400;
+		font-style: italic;
+		font-size: clamp(18px, 2vw, 24px);
+	}
+</style>
