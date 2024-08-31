0faa1ccf7d307feba0a7164ddc10e61b87a73a99
jorj-kare <jorj.kare@gmail.com>
2024-08-31 22:21:29 +0200

    add hooks

---
+++ src/hooks.server.js
@@ -0,0 +1,31 @@
+import User from '$lib/server/models/admin';
+import { connectToDatabase } from '$lib/server/mongoDB.js';
+import { verifyToken } from '$lib/server/utils/jwt';
+import { redirect } from '@sveltejs/kit';
+
+connectToDatabase().then(async () => {
+	console.log('You have successfully connected to the database!');
+});
+
+export const handle = async ({ event, resolve }) => {
+	const jwt = event.cookies.get('jwt');
+	const url = new URL(event.request.url);
+	if (!jwt && url.pathname == '/admin') {
+		throw redirect(302, '/admin/login');
+	}
+	if (jwt) {
+		const decoded = await verifyToken(jwt);
+
+		const user = await User.findById(decoded?.id);
+
+		if (!user && url.pathname == '/admin') {
+			throw redirect(302, '/admin/login');
+		}
+		if (user) {
+			event.locals.user = {
+				user
+			};
+		}
+	}
+	return await resolve(event);
+};
