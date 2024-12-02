de601cf9620259ad6ff2855e1c61b4f8a2ad23eb
jorj-kare <jorj.kare@gmail.com>
2024-12-02 19:31:54 +0100

    modify carousel component

--- src/lib/Carousel.svelte
+++ src/lib/Carousel.svelte
@@ -1,70 +1,125 @@
 <script>
+	import { PUBLIC_CLOUDINARY_URL } from '$env/static/public';
+	import IoIosClose from 'svelte-icons/io/IoIosClose.svelte';
 	import { fade } from 'svelte/transition';
 	import FaArrowRight from 'svelte-icons/fa/FaArrowRight.svelte';
 	import FaArrowLeft from 'svelte-icons/fa/FaArrowLeft.svelte';
-	import GoArrowSmallRight from 'svelte-icons/go/GoArrowSmallRight.svelte';
-	import GoArrowSmallLeft from 'svelte-icons/go/GoArrowSmallLeft.svelte';
+
 	export let images;
 
 	let currentSlideItem = 0;
 	const nextImage = () => {
-		currentSlideItem = (currentSlideItem + 1) % images.length;
+		currentSlideItem = (currentSlideItem + 1) % images.pictures.length;
 	};
 	const prevImage = () => {
 		if (currentSlideItem != 0) {
-			currentSlideItem = (currentSlideItem - 1) % images.length;
+			currentSlideItem = (currentSlideItem - 1) % images.pictures.length;
 		} else {
-			currentSlideItem = images.length - 1;
+			currentSlideItem = images.pictures.length - 1;
 		}
 	};
 </script>
 
 <div class="box">
-	<figure>
-		{#each [images[currentSlideItem]] as item (currentSlideItem)}
+	<figure transition:fade={{ duration: 150 }}>
+		{#each [images.pictures[currentSlideItem]] as item (currentSlideItem)}
+			<div class="close-icon" on:click>
+				<IoIosClose></IoIosClose>
+			</div>
 			<img
 				in:fade={{ duration: 1000 }}
-				src={item.url}
-				alt={item.description}
+				src={PUBLIC_CLOUDINARY_URL + item}
+				alt={images.description}
 				width={1000}
 				height={600}
 			/>
 		{/each}
+		<div class="carousel-buttons">
+			<button class="btn-left" on:click={() => prevImage()}><FaArrowLeft /></button>
+			<button class="btn-right" on:click={() => nextImage()}><FaArrowRight /></button>
+		</div>
 	</figure>
-	<div class="carousel-buttons">
-		<button class="btn-left" on:click={() => prevImage()}><FaArrowLeft /></button>
-		<button class="btn-right" on:click={() => nextImage()}><FaArrowRight /></button>
-	</div>
+	<p>{images.description}</p>
 </div>
 
 <style>
 	.box {
 		display: flex;
 		flex-direction: column;
-		justify-content: end;
+		justify-content: center;
 		align-items: center;
-		width: 50vw;
-		height: 70vh;
+		width: 100vw;
+		min-height: 100vh;
+		background-color: rgb(5, 5, 5);
+	}
+	figure {
+		position: relative;
+		display: grid;
+		grid-template-columns: 1fr 4rem;
+		grid-template-rows: auto auto;
+		align-items: center;
+		justify-content: center;
+		height: 85vh;
+		width: 65vw;
+		margin-top: 15px;
+		border-radius: 15px;
+		color: #e9e6e6;
+		border: 3px solid #e9e6e6;
+	}
+	p {
+		padding: 10px 20px;
+		max-width: 120ch;
+		margin-top: 5px;
+
+		color: #e9e6e6;
 	}
 
 	img {
+		grid-column: 1/2;
+		grid-row: 1/2;
+		width: 100%;
+		height: 100%;
+		padding-top: 50px;
+		padding-left: 15px;
 		object-fit: contain;
-		margin: 10px;
+		overflow: hidden;
+	}
+	.carousel-buttons {
+		grid-row: 2/3;
+		grid-column: 1/2;
+		justify-self: center;
+		align-self: center;
 	}
 	button {
 		width: 50px;
 		height: 25px;
-		margin: 10px;
+		margin: 25px 0px;
 		color: aliceblue;
 		background-color: transparent;
 		border: none;
 		border-radius: 50%;
-		transition: transform 0.5s;
+		transition: all 0.5s;
 	}
 	.btn-right:hover {
 		transform: translateX(3px);
+		color: #1c8437;
 	}
 	.btn-left:hover {
 		transform: translateX(-3px);
+		color: #1c8437;
+	}
+	.close-icon {
+		grid-column: 2/3;
+		grid-row: 1/2;
+		align-self: flex-start;
+		justify-self: end;
+		width: 5.5rem;
+		margin: 5px;
+		color: #fafafa;
+		transition: all 0.5s;
+	}
+	.close-icon:hover {
+		transform: rotate(180deg);
+		color: #660b0b;
 	}
 </style>
