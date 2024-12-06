8e82a6365685b12e1fdaed59dd152d62658acd70
jorj-kare <geoKare@protonmail.com>
2024-12-07 00:13:17 +0100

    modify header at main page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,5 +1,11 @@
 <script>
 	import video from '$lib/media/166808-835670743.mp4';
+	import AnimateHeader from '../../lib/AnimateHeader.svelte';
+	const imgArr = [
+		'https://res.cloudinary.com/dqn25bn8q/image/upload/v1727470059/xilourgio/56dBO1Niw2nQIQYTRukrk.jpg',
+		'https://res.cloudinary.com/dqn25bn8q/image/upload/v1727469634/xilourgio/8DEnGdTULgwPTiyaU3y-b.jpg',
+		'https://res.cloudinary.com/dqn25bn8q/image/upload/v1727469634/xilourgio/2qD__zLmvqeRN6eMyAZLa.jpg'
+	];
 </script>
 
 <div class="container">
@@ -17,57 +23,61 @@
 			</p>
 		</div>
 	</header>
-	<section class="info">
-		<div>
-			<h2>Τι κάνουμε</h2>
-			<ul>
-				<li>
-					Επί παραγγελία κατασκευές: Έπιπλα, ντουλάπες, ράφια, κουζίνες και ό,τι άλλο φανταστείτε –
-					όλα φτιαγμένα με τα χέρια μας και με μεγάλη προσοχή στη λεπτομέρεια.
-				</li>
-				<li>
-					Επισκευές & ανακαινίσεις: Έχετε κάποιο παλιό έπιπλο που χρειάζεται ανανέωση; Εμείς το
-					φέρνουμε ξανά στη ζωή!
-				</li>
-			</ul>
-			<p>
-				Τέλος επειδή στοχεύουμε στην ολοκληρωμένη εξυπηρέτηση σας, σας παρέχουμε τη συνεργασία και
-				την υποστήριξή μας ακόμα και μετά την πώληση.
-			</p>
-		</div>
+	<section>
+		<AnimateHeader imgPathArr={imgArr}>
+			<div class="info">
+				<div>
+					<h2>Τι κάνουμε</h2>
+					<ul>
+						<li>
+							Επί παραγγελία κατασκευές: Έπιπλα, ντουλάπες, ράφια, κουζίνες και ό,τι άλλο
+							φανταστείτε – όλα φτιαγμένα με τα χέρια μας και με μεγάλη προσοχή στη λεπτομέρεια.
+						</li>
+						<li>
+							Επισκευές & ανακαινίσεις: Έχετε κάποιο παλιό έπιπλο που χρειάζεται ανανέωση; Εμείς το
+							φέρνουμε ξανά στη ζωή!
+						</li>
+					</ul>
+					<p>
+						Τέλος επειδή στοχεύουμε στην ολοκληρωμένη εξυπηρέτηση σας, σας παρέχουμε τη συνεργασία
+						και την υποστήριξή μας ακόμα και μετά την πώληση.
+					</p>
+				</div>
 
-		<div>
-			<h2>Γιατί να μας επιλέξετε</h2>
-			<ul>
-				<li>
-					Ποιότητα με προσωπικότητα: Κάθε έργο μας είναι φτιαγμένο με προσοχή και υλικά που
-					διαρκούν.
-				</li>
-				<li>
-					Φαντασία και εμπειρία: Έχουμε χρόνια εμπειρίας και πολλές ιδέες για να κάνουμε το σπίτι
-					σας ή το γραφείο σας να ξεχωρίσει.
-				</li>
-				<li>
-					Γρήγορα και άμεσα: Η δουλειά μας ολοκληρώνεται στην ώρα της, χωρίς να χάνουμε την
-					ποιότητα.
-				</li>
-				<li>
-					Ανοιχτοί σε ιδέες: Είμαστε πάντα ανοιχτοί να ακούσουμε τις ιδέες σας και να δουλέψουμε
-					μαζί για το καλύτερο αποτέλεσμα.
-				</li>
-			</ul>
-		</div>
-		<div>
-			<h2>Δώστε ζωή στον χώρο σας</h2>
-			<p>
-				Αν ψάχνετε κάποιον να φτιάξει ή να ανακαινίσει τον χώρο σας με αγάπη και φροντίδα, μη
-				διστάσετε να επικοινωνήσετε μαζί μας. Είμαστε έτοιμοι να δώσουμε ζωή στα σχέδιά σας!
-			</p>
-			<p>
-				Η κάθε λύση που προτείνουμε συνδυάζει λειτουργικότητα, εργονομία και αισθητική χωρίς να της
-				λείπει ταυτόχρονα και η ποιότητα.
-			</p>
-		</div>
+				<div>
+					<h2>Γιατί να μας επιλέξετε</h2>
+					<ul>
+						<li>
+							Ποιότητα με προσωπικότητα: Κάθε έργο μας είναι φτιαγμένο με προσοχή και υλικά που
+							διαρκούν.
+						</li>
+						<li>
+							Φαντασία και εμπειρία: Έχουμε χρόνια εμπειρίας και πολλές ιδέες για να κάνουμε το
+							σπίτι σας ή το γραφείο σας να ξεχωρίσει.
+						</li>
+						<li>
+							Γρήγορα και άμεσα: Η δουλειά μας ολοκληρώνεται στην ώρα της, χωρίς να χάνουμε την
+							ποιότητα.
+						</li>
+						<li>
+							Ανοιχτοί σε ιδέες: Είμαστε πάντα ανοιχτοί να ακούσουμε τις ιδέες σας και να δουλέψουμε
+							μαζί για το καλύτερο αποτέλεσμα.
+						</li>
+					</ul>
+				</div>
+				<div>
+					<h2>Δώστε ζωή στον χώρο σας</h2>
+					<p>
+						Αν ψάχνετε κάποιον να φτιάξει ή να ανακαινίσει τον χώρο σας με αγάπη και φροντίδα, μη
+						διστάσετε να επικοινωνήσετε μαζί μας. Είμαστε έτοιμοι να δώσουμε ζωή στα σχέδιά σας!
+					</p>
+					<p>
+						Η κάθε λύση που προτείνουμε συνδυάζει λειτουργικότητα, εργονομία και αισθητική χωρίς να
+						της λείπει ταυτόχρονα και η ποιότητα.
+					</p>
+				</div>
+			</div>
+		</AnimateHeader>
 	</section>
 </div>
 
@@ -76,7 +86,6 @@
 		scroll-snap-type: y mandatory;
 		height: 100vh;
 		overflow-y: scroll;
-
 		word-spacing: -2px;
 	}
 	header {
@@ -115,18 +124,19 @@
 		font-size: 4rem;
 		border-bottom: 2px solid #f3f2f2;
 	}
-
-	.info {
+	section {
 		position: relative;
-		display: grid;
+		z-index: 11;
+	}
+	.info {
+		display: flex;
 		grid-template-columns: 1fr 1fr 1fr;
 		align-items: flex-end;
 		align-content: end;
 		gap: 4rem;
 		padding: 30px;
-		min-height: 100vh;
+		height: 100vh;
 		border-top: 2px solid #f3f2f2;
-		animation: slideshow 10s infinite;
 		background-size: cover;
 		background-repeat: no-repeat;
 		background-position: center;
@@ -150,44 +160,5 @@
 	}
 	.info p {
 		padding-top: 5px;
-	}
-
-	/* ------- ΜΕ ΕΝΑΛΛΑΓΗ ΕΙΚΟΝΩΝ --------------  */
-	@keyframes slideshow {
-		0%,
-		100% {
-			background-image: linear-gradient(rgba(0, 0, 0, 0.569), rgba(0, 0, 0, 0.569)),
-				url(https://res.cloudinary.com/dqn25bn8q/image/upload/v1727470059/xilourgio/zbmhllgruxu4su6innkf.jpg);
-		}
-		24.99% {
-			background-image: linear-gradient(rgba(0, 0, 0, 0.569), rgba(0, 0, 0, 0.569)),
-				url(https://res.cloudinary.com/dqn25bn8q/image/upload/v1727470059/xilourgio/zbmhllgruxu4su6innkf.jpg);
-		}
-
-		25% {
-			background-image: linear-gradient(rgba(0, 0, 0, 0.569), rgba(0, 0, 0, 0.569)),
-				url(https://res.cloudinary.com/dqn25bn8q/image/upload/v1727469701/xilourgio/fNmAyKPF9xRxSvd37UXFe.jpg);
-		}
-		49.99% {
-			background-image: linear-gradient(rgba(0, 0, 0, 0.569), rgba(0, 0, 0, 0.569)),
-				url(https://res.cloudinary.com/dqn25bn8q/image/upload/v1727469701/xilourgio/fNmAyKPF9xRxSvd37UXFe.jpg);
-		}
-
-		50% {
-			background-image: linear-gradient(rgba(0, 0, 0, 0.569), rgba(0, 0, 0, 0.569)),
-				url(https://res.cloudinary.com/dqn25bn8q/image/upload/v1727469634/xilourgio/zbmhllgruxu4su6innkf.jpg);
-		}
-		74.99% {
-			background-image: linear-gradient(rgba(0, 0, 0, 0.569), rgba(0, 0, 0, 0.569)),
-				url(https://res.cloudinary.com/dqn25bn8q/image/upload/v1727469634/xilourgio/zbmhllgruxu4su6innkf.jpg);
-		}
-		75% {
-			background-image: linear-gradient(rgba(0, 0, 0, 0.569), rgba(0, 0, 0, 0.569)),
-				url(https://res.cloudinary.com/dqn25bn8q/image/upload/v1727469634/xilourgio/fNmAyKPF9xRxSvd37UXFe.jpg);
-		}
-		99.99% {
-			background-image: linear-gradient(rgba(0, 0, 0, 0.569), rgba(0, 0, 0, 0.569)),
-				url(https://res.cloudinary.com/dqn25bn8q/image/upload/v1727469634/xilourgio/fNmAyKPF9xRxSvd37UXFe.jpg);
-		}
 	}
 </style>
