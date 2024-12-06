eb78058f3e105ce90877d0f6d8d7ccb3f340d57c
jorj-kare <geoKare@protonmail.com>
2024-12-06 13:37:12 +0100

    debug restricting routes

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -3,6 +3,8 @@ import { connectToDatabase } from '$lib/server/mongoDB.js';
 import { verifyToken } from '$lib/server/utils/jwt';
 import { redirect } from '@sveltejs/kit';
 
+const restrictedPaths = ['/admin', '/admin/create', '/admin/edit'];
+
 connectToDatabase().then(async () => {
 	console.log('You have successfully connected to the database!');
 });
@@ -10,7 +12,7 @@ connectToDatabase().then(async () => {
 export const handle = async ({ event, resolve }) => {
 	const jwt = event.cookies.get('jwt');
 	const url = new URL(event.request.url);
-	if (!jwt && url.pathname == '/admin') {
+	if (!jwt && restrictedPaths.includes(url.pathname.split('/', 3).join('/'))) {
 		throw redirect(302, '/admin/login');
 	}
 	if (jwt) {
